6722d786fac1a7b5ebdb6d890fca8a2f3b0a1aa1
Ibnu Muhammad <ibnu.muhammad@rumahdaud.com>
2023-08-26 19:11:56 +0700

    Cleanup code src/main/java/org/davidcampos/kafka/cli/Main.java

--- src/main/java/org/davidcampos/kafka/cli/Main.java
+++ src/main/java/org/davidcampos/kafka/cli/Main.java
@@ -1,6 +1,5 @@
 package org.davidcampos.kafka.cli;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.davidcampos.kafka.commons.Commons;
@@ -13,8 +12,9 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(final String... args) {
-        String EXAMPLE_GOAL = System.getenv("EXAMPLE_GOAL") != null ?
-                System.getenv("EXAMPLE_GOAL") : "producer";
+        String EXAMPLE_GOAL = System.getenv("EXAMPLE_GOAL") != null
+                ? System.getenv("EXAMPLE_GOAL")
+                : "producer";
 
         logger.info("Kafka Topic: {}", Commons.EXAMPLE_KAFKA_TOPIC);
         logger.info("Kafka Server: {}", Commons.EXAMPLE_KAFKA_SERVER);
@@ -22,21 +22,21 @@ public class Main {
         logger.info("GOAL: {}", EXAMPLE_GOAL);
 
         switch (EXAMPLE_GOAL.toLowerCase()) {
-            case "producer":
-                KafkaProducerExample.main();
-                break;
-            case "consumer.kafka":
-                KafkaConsumerExample.main();
-                break;
-            case "consumer.spark":
-                KafkaSparkConsumerExample.main();
-                break;
-            case "consumer.flink":
-                KafkaFlinkConsumerExample.main();
-                break;
-            default:
-                logger.error("No valid goal to run.");
-                break;
+        case "producer":
+            KafkaProducerExample.main();
+            break;
+        case "consumer.kafka":
+            KafkaConsumerExample.main();
+            break;
+        case "consumer.spark":
+            KafkaSparkConsumerExample.main();
+            break;
+        case "consumer.flink":
+            KafkaFlinkConsumerExample.main();
+            break;
+        default:
+            logger.error("No valid goal to run.");
+            break;
         }
     }
 }
