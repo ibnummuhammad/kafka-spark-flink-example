bd68cd205cbd8081bd65f1ec13d5fedd53ecaacc
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-08-26 19:57:06 +0700

    Cleanup code src/main/java/org/davidcampos/kafka/consumer/KafkaConsumerExample.java

--- src/main/java/org/davidcampos/kafka/consumer/KafkaConsumerExample.java
+++ src/main/java/org/davidcampos/kafka/consumer/KafkaConsumerExample.java
@@ -9,7 +9,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.davidcampos.kafka.commons.Commons;
 
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +17,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class KafkaConsumerExample {
-    private static final Logger logger = LogManager.getLogger(KafkaConsumerExample.class);
+    private static final Logger logger = LogManager
+            .getLogger(KafkaConsumerExample.class);
 
     public static void main(final String... args) {
         ConcurrentMap<String, Integer> counters = new ConcurrentHashMap<>();
@@ -40,10 +40,13 @@ public class KafkaConsumerExample {
     private static Consumer<String, String> createConsumer() {
         // Create properties
         final Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Commons.EXAMPLE_KAFKA_SERVER);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                Commons.EXAMPLE_KAFKA_SERVER);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaConsumerGroup");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
 
         // Create the consumer using properties
         final Consumer<String, String> consumer = new KafkaConsumer(props);
