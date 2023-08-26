90eabd7d2f5dc3efe9fba5731b591e5c0ca81325
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-08-26 19:45:51 +0700

    Cleanup code src/main/java/org/davidcampos/kafka/consumer/KafkaFlinkConsumerExample.java

--- src/main/java/org/davidcampos/kafka/consumer/KafkaFlinkConsumerExample.java
+++ src/main/java/org/davidcampos/kafka/consumer/KafkaFlinkConsumerExample.java
@@ -1,6 +1,5 @@
 package org.davidcampos.kafka.consumer;
 
-
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -16,26 +15,27 @@ import org.davidcampos.kafka.commons.Commons;
 import java.util.Properties;
 
 public class KafkaFlinkConsumerExample {
-    private static final Logger logger = LogManager.getLogger(KafkaFlinkConsumerExample.class);
+    private static final Logger logger = LogManager
+            .getLogger(KafkaFlinkConsumerExample.class);
 
     public static void main(final String... args) {
         // Create execution environment
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment
+                .getExecutionEnvironment();
 
         // Properties
         final Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Commons.EXAMPLE_KAFKA_SERVER);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                Commons.EXAMPLE_KAFKA_SERVER);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "FlinkConsumerGroup");
 
-        DataStream<String> messageStream = env.addSource(new FlinkKafkaConsumer010<>(Commons.EXAMPLE_KAFKA_TOPIC, new SimpleStringSchema(), props));
-
+        DataStream<String> messageStream = env.addSource(new FlinkKafkaConsumer010<>(
+                Commons.EXAMPLE_KAFKA_TOPIC, new SimpleStringSchema(), props));
 
         // Split up the lines in pairs (2-tuples) containing: (word,1)
         messageStream.flatMap(new Tokenizer())
                 // group by the tuple field "0" and sum up tuple field "1"
-                .keyBy(0)
-                .sum(1)
-                .print();
+                .keyBy(0).sum(1).print();
 
         try {
             env.execute();
@@ -44,7 +44,8 @@ public class KafkaFlinkConsumerExample {
         }
     }
 
-    public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+    public static final class Tokenizer
+            implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
         @Override
         public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
