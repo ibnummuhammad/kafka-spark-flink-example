1ea2f52d4ecf444e144f65a197809aefbd9dc208
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-09-04 19:17:35 +0700

    Set org.apache.spark spark-streaming-kafka-0-10_2.11 to 2.4.8

--- src/main/java/org/davidcampos/kafka/consumer/KafkaSparkConsumerExample.java
+++ src/main/java/org/davidcampos/kafka/consumer/KafkaSparkConsumerExample.java
@@ -29,7 +29,7 @@ public class KafkaSparkConsumerExample {
             .getLogger(KafkaSparkConsumerExample.class);
 
     public static void main(final String... args) {
-        String cetak = "Remove dependency com.fasterxml.jackson.module jackson-module-scala_2.11 2.9.5";
+        String cetak = "Set org.apache.spark spark-streaming-kafka-0-10_2.11 to 2.4.8";
         logger.info(cetak);
         System.out.println(cetak);
 
