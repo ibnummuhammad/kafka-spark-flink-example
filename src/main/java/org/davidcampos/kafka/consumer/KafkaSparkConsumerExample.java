dc9fe0c8f59158716fa6ac4e05ff83aa7bd3fc1f
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-09-04 21:45:04 +0700

    Revamp modules org.apache.spark.streaming.api.java

--- src/main/java/org/davidcampos/kafka/consumer/KafkaSparkConsumerExample.java
+++ src/main/java/org/davidcampos/kafka/consumer/KafkaSparkConsumerExample.java
@@ -21,7 +21,10 @@ import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
-import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.Durations;
 
 public class KafkaSparkConsumerExample {
@@ -29,7 +32,7 @@ public class KafkaSparkConsumerExample {
             .getLogger(KafkaSparkConsumerExample.class);
 
     public static void main(final String... args) {
-        String cetak = "Set org.apache.spark spark-streaming-kafka-0-10_2.11 version to 2.4.8";
+        String cetak = "Revamp modules org.apache.spark.streaming.api.java";
         logger.info(cetak);
         System.out.println(cetak);
 
